322df24ac1978a62a0116606b32ac54870c18060
unknown <hei.marius.3@gmail.com>
2025-07-13 00:36:50 +0300

    feat: save user methode and existByEmail methode for register condition

--- src/main/java/com/file/share/repository/UserRepository.java
+++ src/main/java/com/file/share/repository/UserRepository.java
@@ -20,19 +20,44 @@ public class UserRepository {
         String query = "SELECT * FROM users WHERE email = ?";
         try (Connection connection = dbConnector.getConnection();
              PreparedStatement preparedStatement= connection.prepareStatement(query)){
-            preparedStatement.setString(1,email);
-            ResultSet resultSet = preparedStatement.executeQuery();
+             preparedStatement.setString(1,email);
+             ResultSet resultSet = preparedStatement.executeQuery();
 
-            if (resultSet.next()){
-                return new User(
+             if (resultSet.next()){
+                 return new User(
                         (UUID) resultSet.getObject("id"),
                         resultSet.getString("email"),
                         resultSet.getString("password")
-                );
-            }
-            return null;
+                 );
+             }
+             return null;
         } catch (SQLException e){
             throw new RuntimeException();
+        }
+    }
+
+    public void save(User user) {
+        String query = "INSERT INTO users (email,password) VALUES (?,?)";
+        try (PreparedStatement statement = dbConnector.getConnection().prepareStatement(query)) {
+            statement.setString(1, user.getEmail());
+            statement.setString(2, user.getPassword());
+
+            statement.executeUpdate();
+        } catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean existByEmail(String email){
+        String query = "SELECT 1 FROM users WHERE email = ?";
+        try (PreparedStatement statement = dbConnector.getConnection().prepareStatement(query)) {
+            statement.setString(1,email);
+            try(ResultSet resultSet = statement.executeQuery()) {
+                return resultSet.next();
+            }
+
+        } catch (SQLException e){
+            throw new RuntimeException(e);
         }
     }
 }
