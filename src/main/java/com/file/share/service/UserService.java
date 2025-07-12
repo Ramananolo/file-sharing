6f00f35ffa228af0b6f1752f1624601b66d94aa5
unknown <hei.marius.3@gmail.com>
2025-07-13 00:39:02 +0300

    feat: repository of user register

--- src/main/java/com/file/share/service/UserService.java
+++ src/main/java/com/file/share/service/UserService.java
@@ -1,4 +1,18 @@
 package com.file.share.service;
 
+import com.file.share.repository.UserRepository;
+import com.file.share.repository.model.User;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    public void register(User user){
+        if (userRepository.existByEmail(user.getEmail()))
+            throw new RuntimeException("Email already exist");
+        userRepository.save(user);
+    }
 }
