5433eca66afe690e7bd82db91e7445bab23bcb9b
unknown <hei.marius.3@gmail.com>
2025-07-13 02:25:50 +0300

    refactor: apply password encoder for the pw new user add

--- src/main/java/com/file/share/service/UserService.java
+++ src/main/java/com/file/share/service/UserService.java
@@ -3,16 +3,21 @@ package com.file.share.service;
 import com.file.share.repository.UserRepository;
 import com.file.share.repository.model.User;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public void register(User user){
         if (userRepository.existByEmail(user.getEmail()))
             throw new RuntimeException("Email already exist");
+
+        String encoderPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encoderPassword);
         userRepository.save(user);
     }
 }
