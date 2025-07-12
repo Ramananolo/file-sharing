f6461e5ba9aa780428195567ce065314a16d581e
unknown <hei.marius.3@gmail.com>
2025-07-13 00:33:18 +0300

    feat: RegisterUser controller

--- src/main/java/com/file/share/endpoint/UserController.java
+++ src/main/java/com/file/share/endpoint/UserController.java
@@ -1,11 +1,27 @@
-package com.file.share.endpoint.authRequestBody;
+package com.file.share.endpoint;
 
+import com.file.share.repository.model.User;
+import com.file.share.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("users")
+@RequestMapping("/register")
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<String> RegisterUser(@RequestBody User user){
+        try {
+            userService.register(user);
+            return ResponseEntity.ok("User register successfully");
+        } catch (RuntimeException e){
+            return ResponseEntity.badRequest().body("Error: "+ e.getMessage());
+        }
+    }
 }
